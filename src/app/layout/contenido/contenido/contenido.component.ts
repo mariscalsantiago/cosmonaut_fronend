9f4aafbecbd973a439619aa5c0d9d35327cd22eb
santiago antonio <santiagoantoniomariscal@gmail.com>
2021-01-08 18:14:13 -0600

    select correct template

--- src/app/layout/contenido/contenido/contenido.component.ts
+++ src/app/layout/contenido/contenido/contenido.component.ts
@@ -7,10 +7,59 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContenidoComponent implements OnInit {
 
-  
+  public arreglo: Array<menuprincipal> = [
+    { nombre: "INICIO", icono: "icon_home", seleccionado: true,seleccionadosubmenu:false },
+    { nombre: "EMPLEADOS", icono: "icon_nominas", seleccionado: false ,seleccionadosubmenu:false},
+    { nombre: "NOMINAS", icono: "icon_empleados", seleccionado: false,seleccionadosubmenu:false,submenu:[
+                                                                                {nombre:"COMPLETAR"},
+                                                                                {nombre:"LISTA"},
+                                                                                {nombre:"DAR DE ALTA"},
+                                                                                {nombre:"DAR DE BAJA"},
+                                                                                {nombre:"DIRECTORIO"}
+                                                                              ] },
+    { nombre: "EVENTOS", icono: "icon_eventos", seleccionado: false ,seleccionadosubmenu:false},
+    { nombre: "IMSS", icono: "icon_imss", seleccionado: false,seleccionadosubmenu:false },
+    { nombre: "REPORTES", icono: "icon_reportes", seleccionado: false,seleccionadosubmenu:false },
+    { nombre: "MI PERFIL", icono: "icon_perfil", seleccionado: false ,seleccionadosubmenu:false}
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  public limpiando(){
+    for (let item of this.arreglo)
+      item.seleccionado = false;
+  }
+
+  public seleccionado(obj: any) {
+    this.limpiando();
+
+    obj.seleccionado = true;
+
+    obj.seleccionadosubmenu = !obj.seleccionadosubmenu;
+  }
+
+
+  public seleccionarSubmenu(obj:any,obj2:any)  {
+    this.limpiando();
+    obj.seleccionado = true;
+
+    
+  }
+
+}
+
+
+interface menuprincipal {
+  nombre: string;
+  icono: string;
+  seleccionado: boolean,
+  seleccionadosubmenu:boolean,
+  submenu?: Array<submenu>;
+}
+
+interface submenu {
+  nombre: string;
 }
