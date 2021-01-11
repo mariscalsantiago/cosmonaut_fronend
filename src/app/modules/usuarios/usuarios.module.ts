3262b362724615c1f3cdfc829d48b9ca35956769
santiago antonio <santiagoantoniomariscal@gmail.com>
2021-01-11 12:06:01 -0600

    modularización componente detalle-usuario y usuario ..

--- src/app/modules/usuarios/usuarios.module.ts
+++ src/app/modules/usuarios/usuarios.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { UsuariosComponent } from './pages/usuarios.component';
 import { usuariosRoutingModule } from './usuarios-routing.module';
 import { ShareModule } from 'src/app/shared/share.module';
+import { DetalleUsuarioComponent } from './pages/detalle-usuario/detalle-usuario.component';
 
 
 
 @NgModule({
-  declarations: [UsuariosComponent],
+  declarations: [DetalleUsuarioComponent, DetalleUsuarioComponent],
   imports: [
     CommonModule,
     usuariosRoutingModule,
