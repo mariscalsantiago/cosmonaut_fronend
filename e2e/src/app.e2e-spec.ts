b6a22bf4a9c59da1c7335784f038a36e6b925197
Isidoro Trevino <isidoro.trevino@gmail.com>
2021-01-21 12:23:22 -0600

    Integración de pruebas con protractor

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,14 +1,13 @@
 import { AppPage } from './app.po';
-import { browser, logging } from 'protractor';
-
-xdescribe('workspace-project App', () => {
+import { browser, by, element, logging } from 'protractor';
+/*describe('workspace-project App', () => {
   let page: AppPage;
 
   beforeEach(() => {
     page = new AppPage();
   });
 
-  xit('should display welcome message', async () => {
+  it('should display welcome message', async () => {
     await page.navigateTo();
     expect(await page.getTitleText()).toEqual('cosmonaut-front app is running!');
   });
@@ -20,4 +19,30 @@ xdescribe('workspace-project App', () => {
       level: logging.Level.SEVERE,
     } as logging.Entry));
   });
+});*/
+describe('workspace-project App', () => {
+  it('Login exitoso', () => {
+    browser.get('http://localhost:4200');
+    element(by.id('exampleInputEmail1')).sendKeys('juan@perez.com');
+    element(by.id('exampleInputPassword1')).sendKeys('123456789');
+    element(by.css('button')).click().then(() => {
+      expect(element(by.css('body')).getText()).toContain('inicio works!');
+    });
+
+    afterEach(async () => {
+      const logs = await browser.manage().logs().get(logging.Type.BROWSER);
+      expect(logs).not.toContain(jasmine.objectContaining({
+        level: logging.Level.SEVERE,
+      } as logging.Entry));
+    });
+
+  });
+  let page: AppPage;
+
+  beforeEach(() => {
+      page = new AppPage();
+    }
+  );
+
+
 });
