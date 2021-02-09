2e1cdcfa4e32f13b44a0a53db16bd936a1d6d3bc
santiago mariscal <santiago.mariscal@advisoryservicesg.tech>
2021-02-09 07:47:30 -0600

    empleados y compania logo listos

--- src/app/modules/empleados/empleados.module.ts
+++ src/app/modules/empleados/empleados.module.ts
@@ -4,14 +4,22 @@ import { empleadosRoutingModule } from './empleados-routing.module';
 import { ListaempleadosComponent } from './pages/listaempleados/listaempleados.component';
 import { EmpleadoComponent } from './pages/empleado/empleado.component';
 import { ShareModule } from 'src/app/shared/share.module';
+import { FormEmpleadoComponent } from './pages/form-empleado/form-empleado.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { InformacionbasicaComponent } from './pages/form-empleado/pestañas/informacionbasica/informacionbasica.component';
+import { DomicilioComponent } from './pages/form-empleado/pestañas/domicilio/domicilio.component';
+import { PreferenciasComponent } from './pages/form-empleado/pestañas/preferencias/preferencias.component';
 
 
 @NgModule({
-    declarations: [ListaempleadosComponent, EmpleadoComponent],
+    declarations: [ListaempleadosComponent, EmpleadoComponent, FormEmpleadoComponent, InformacionbasicaComponent, DomicilioComponent, PreferenciasComponent],
     imports: [
       CommonModule,
       empleadosRoutingModule,
-      ShareModule
+      ShareModule,
+      FormsModule,
+      ReactiveFormsModule
     ],
     providers: []
   })
---
+++ src/app/modules/empleados/pages/form-empleado/pestañas/informacionbasica/informacionbasica.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-informacionbasica',
+  templateUrl: './informacionbasica.component.html',
+  styleUrls: ['./informacionbasica.component.scss']
+})
+export class InformacionbasicaComponent implements OnInit {
+
+  public arreglopintar: any = [false];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+
+  public cambiarStatus(valor: any) {
+
+    for (let x = 0; x < this.arreglopintar.length; x++) {
+
+      if (x == valor) {
+        continue;
+      }
+
+      this.arreglopintar[x] = false;
+
+    }
+
+    this.arreglopintar[valor] = !this.arreglopintar[valor];
+
+
+    console.log(this.arreglopintar);
+  }
+}
