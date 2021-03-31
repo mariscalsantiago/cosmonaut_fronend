c6c4e81eb040e81b269b4314838697dff501df22
santiago mariscal <santiago.mariscal@advisoryservicesg.tech>
2021-03-31 15:02:35 -0600

    evento calendairo

--- src/app/modules/eventos/pages/calendario/calendario.component.ts
+++ src/app/modules/eventos/pages/calendario/calendario.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { tabla } from 'src/app/core/data/tabla';
+import { VentanaemergenteService } from 'src/app/shared/services/modales/ventanaemergente.service';
 
 @Component({
   selector: 'app-calendarioevento',
@@ -7,11 +9,61 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CalendarioComponent implements OnInit {
 
-  public apareceListadoEventos:boolean = false;
+  public apareceListadoEventos: boolean = false;
+  public arreglotabla:any = {
+    columnas:[],
+    filas:[]
+  }
 
-  constructor() { }
+  constructor(private ventana:VentanaemergenteService) { }
 
   ngOnInit(): void {
+
+    const columnas:Array<tabla> = [
+      new tabla("tipo","Tipo"),
+      new tabla("nombre","Empleado"),
+      new tabla("x","Fecha de inicio"),
+      new tabla("x","Fecha fin"),
+      new tabla("x","Duración"),
+      new tabla("x","Estado")
+    ];
+
+    let arreglo = [];
+    arreglo.push({tipo:"1",nombre:'santiago'});
+    arreglo.push({tipo:"1",nombre:'santiago'});
+    arreglo.push({tipo:"1",nombre:'santiago'});
+    arreglo.push({tipo:"1",nombre:'santiago'});
+    arreglo.push({tipo:"1",nombre:'santiago'});
+    arreglo.push({tipo:"1",nombre:'santiago'});
+    arreglo.push({tipo:"1",nombre:'santiago'});
+    arreglo.push({tipo:"1",nombre:'santiago'});
+    arreglo.push({tipo:"1",nombre:'santiago'});
+    arreglo.push({tipo:"1",nombre:'santiago'});
+    arreglo.push({tipo:"1",nombre:'santiago'});
+
+    this.arreglotabla.columnas = columnas;
+    this.arreglotabla.filas = arreglo;
+
+  }
+
+  public agregarEvento(indice: number) {
+    switch (indice) {
+      case 1:
+          this.ventana.showVentana(this.ventana.solicitudVacaciones);
+        break;
+      case 2:
+        this.ventana.showVentana(this.ventana.solicitudIncapacidad);
+        break;
+      case 3:
+        this.ventana.showVentana(this.ventana.solicitudHorasExtras);
+        break;
+      case 4:
+        this.ventana.showVentana(this.ventana.solicitudDiasEconomicos);
+        break;
+      case 5:
+        this.ventana.showVentana(this.ventana.registrofaltas);
+        break;
+    }
   }
 
 }
