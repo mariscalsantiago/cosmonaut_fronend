2f0840ed587a9662931d68c4492cf3d6be87c333
santiago mariscal <santiago.mariscal@advisoryservicesg.tech>
2021-04-05 19:56:29 -0500

    agregar eventos por empleado

--- src/app/modules/eventos/pages/eventosxempleado/detalleeventoxempleado/detalleeventoxempleado.component.ts
+++ src/app/modules/eventos/pages/eventosxempleado/detalleeventoxempleado/detalleeventoxempleado.component.ts
@@ -1,10 +1,13 @@
+import { DatePipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { ContratocolaboradorService } from '../../../../empleados/services/contratocolaborador.service';
 import { CatalogosService } from 'src/app/shared/services/catalogos/catalogos.service';
 import { ModalService } from 'src/app/shared/services/modales/modal.service';
 import { UsuarioSistemaService } from 'src/app/shared/services/usuariosistema/usuario-sistema.service';
 import { EmpleadosService } from '../../../../empleados/services/empleados.service';
+import { EventosService } from '../../../services/eventos.service';
 
 @Component({
   selector: 'app-detalleeventoxempleado',
@@ -20,9 +23,12 @@ export class DetalleeventoxempleadoComponent implements OnInit {
   public arregloTipoIncapacidad: any = [];
 
   constructor(private modalPrd: ModalService, private catalogosPrd: CatalogosService, private formbuilder: FormBuilder, private usuarioSistemaPrd: UsuarioSistemaService,
-    private empleadosPrd: EmpleadosService, private router: Router) { }
+    private empleadosPrd: EmpleadosService, private router: Router, private eventoPrd: EventosService,
+    private contratoPrd:ContratocolaboradorService) { }
 
   ngOnInit(): void {
+
+
 
     this.myForm = this.createForms({});
     this.catalogosPrd.getTipoIncidencia(true).subscribe(datos => this.arregloIncidenciaTipo = datos.datos);
@@ -39,18 +45,21 @@ export class DetalleeventoxempleadoComponent implements OnInit {
   }
 
   public createForms(obj: any) {
+    var datePipe = new DatePipe("es-MX");
     return this.formbuilder.group({
       incidenciaId: ['', [Validators.required]],
       personaId: ['', [Validators.required]],
-      duracion: ['', Validators.required],
-      fechaInicio: ['', Validators.required],
+      duracion: [0, Validators.required],
+      fechaInicio: [datePipe.transform(new Date(), "yyyy-MM-dd"), Validators.required],
       monto: [0, [Validators.required]],
-      fechaFin: ['', Validators.required],
+      fechaFin: [{ value: datePipe.transform(new Date(), "yyyy-MM-dd"), disabled: true }, Validators.required],
       fechaAplicacion: ['', Validators.required],
       tipoIncapacidadId: ['', Validators.required],
       urlArchivo: [''],
       numeroFolio: ['', Validators.required],
-      comentarios: ['']
+      comentarios: [''],
+      identificadorPersona: [''],
+      fechaContrato:['']
 
     });
   }
@@ -69,8 +78,8 @@ export class DetalleeventoxempleadoComponent implements OnInit {
 
 
 
-    this.modalPrd.showMessageDialog(this.modalPrd.warning,"¿Deseas registrar el evento?").then(valor =>{
-      if(valor){
+    this.modalPrd.showMessageDialog(this.modalPrd.warning, "¿Deseas registrar el evento?").then(valor => {
+      if (valor) {
 
         this.guardarEvento();
 
@@ -78,20 +87,58 @@ export class DetalleeventoxempleadoComponent implements OnInit {
 
       }
     });;
-  
+
 
   }
 
 
-  public guardarEvento(){
-    let obj = this.myForm.value;
+  public guardarEvento() {
 
-    let objEnviar = {
-      ...obj,
-      tipoIncapacidadId:{
-        tipoIncapacidadId:obj.tipoIncapacidadId
+    let obj = this.myForm.getRawValue();
+    this.modalPrd.showMessageDialog(this.modalPrd.loading);
+
+    this.contratoPrd.getContratoColaboradorById(obj.identificadorPersona).subscribe(contrato =>{
+      console.log(contrato);
+      alert("Si encontro el contrato");
+      if(contrato.resultado){
+        if (obj.fechaInicio != undefined || obj.fechaInicio != '') {
+          obj.fechaInicio = new Date((new Date(obj.fechaInicio).toUTCString()).replace(" 00:00:00 GMT", "")).getTime();
+        }
+    
+        if (obj.fechaFin != undefined || obj.fechaFin != '') {
+          obj.fechaFin = new Date((new Date(obj.fechaFin).toUTCString()).replace(" 00:00:00 GMT", "")).getTime();
+        }
+    
+        if (obj.fechaAplicacion != undefined || obj.fechaAplicacion != '') {
+          obj.fechaAplicacion = new Date((new Date(obj.fechaAplicacion).toUTCString()).replace(" 00:00:00 GMT", "")).getTime();
+        }
+
+        let objEnviar = {
+          ...obj,
+          tipoIncapacidadId: {
+            tipoIncapacidadId: obj.tipoIncapacidadId
+          },
+          tipoIncidenciaId: {
+            tipoIncidenciaId: obj.incidenciaId
+          },
+          personaId:obj.identificadorPersona,
+          clienteId: this.usuarioSistemaPrd.getIdEmpresa(),
+          fechaContrato:contrato.datos.fechaContrato
+        }
+
+
+        this.eventoPrd.save(objEnviar).subscribe(datos => {
+          this.modalPrd.showMessageDialog(this.modalPrd.loadingfinish);
+          this.modalPrd.showMessageDialog(datos.resultado, datos.mensaje).then(() => {
+            if (datos.resultado) {
+              this.router.navigate(['/eventos/eventosxempleado']);
+            }
+          });
+        })
+    
       }
-    }
+    });
+
   }
 
 
@@ -110,11 +157,11 @@ export class DetalleeventoxempleadoComponent implements OnInit {
 
     let seleccionado = Number(this.myForm.controls.incidenciaId.value);
 
-    
 
+    var datePipe = new DatePipe("es-MX");
     switch (cadena) {
       case "dias":
-        console.log("Esto es el valor", this.myForm.controls.incidenciaId.value);
+
         switch (seleccionado) {
           case 1:
           case 2:
@@ -126,7 +173,6 @@ export class DetalleeventoxempleadoComponent implements OnInit {
             ocultar = false;
             break;
         }
-        console.log(ocultar, "Esto se oculta");
         break;
       case "fechainicio":
 
@@ -167,25 +213,76 @@ export class DetalleeventoxempleadoComponent implements OnInit {
     return ocultar;
   }
 
-  public configurandoRestricciones(){
+  public configurandoRestricciones() {
 
     let seleccionado = Number(this.myForm.controls.incidenciaId.value);
-    for(let llave in this.myForm.controls){
-      this.myForm.controls[llave].setErrors(null);
+    for (let llave in this.myForm.controls) {
+      if (llave == "incidenciaId" || llave == "personaId")
+        continue;
+
+
+      this.myForm.controls[llave].clearValidators();
+      this.myForm.controls[llave].updateValueAndValidity();
     }
-   
-      
 
-      this.myForm.controls.incidenciaId.setErrors({required:true});
-      this.myForm.controls.personaId.setErrors({required:true});
-      
 
-      if(seleccionado == 1 || seleccionado == 2 || seleccionado == 5){
-        this.myForm.controls.duracion.setErrors({required:true});
-        this.myForm.controls.fechaInicio.setErrors({required:true});
-        this.myForm.controls.fechaFin.setErrors({required:true});
-        this.myForm.controls.fechaAplicacion.setErrors({required:true});
+
+
+
+
+    if (seleccionado == 1 || seleccionado == 2 || seleccionado == 5) {
+      this.myForm.controls.duracion.setValidators([Validators.required]);
+      this.myForm.controls.duracion.updateValueAndValidity();
+      this.myForm.controls.fechaInicio.setValidators([Validators.required]);
+      this.myForm.controls.fechaInicio.updateValueAndValidity();
+      this.myForm.controls.fechaFin.setValidators([Validators.required]);
+      this.myForm.controls.fechaFin.updateValueAndValidity();
+      this.myForm.controls.fechaAplicacion.setValidators([Validators.required]);
+      this.myForm.controls.fechaAplicacion.updateValueAndValidity();
+    }
+  }
+
+
+  public perderFoco() {
+    const totalDias = this.myForm.controls.duracion.value;
+    if (totalDias !== undefined || totalDias !== null) {
+      var datePipe = new DatePipe("es-MX");
+      const fechaActual = new Date((new Date(this.myForm.controls.fechaInicio.value)).toUTCString().replace("GMT", ""));
+      const fechaFin = new Date(fechaActual.getFullYear(), fechaActual.getMonth(), fechaActual.getDate() + Number(totalDias));
+
+      this.myForm.controls.fechaFin.setValue(datePipe.transform(fechaFin, "yyyy-MM-dd"));
+    }
+  }
+
+
+  public salirPersonaid() {
+    this.myForm.controls.personaId.clearValidators();
+    this.myForm.controls.personaId.updateValueAndValidity();
+
+    this.myForm.value.identificadorPersona = undefined;
+    const nombreCapturado = this.myForm.value.personaId;
+    if (nombreCapturado !== undefined) {
+      if (nombreCapturado.trim() !== "") {
+        let encontrado: boolean = false;
+        for (let item of this.arregloEmpleados) {
+          const nombreCompleto = item.nombre + " " + item.apellidoPaterno;
+          if (nombreCompleto.includes(nombreCapturado)) {
+            encontrado = true;
+            this.myForm.controls.identificadorPersona.setValue(item.idPersona);
+            this.myForm.controls.fechaContrato.setValue(item.fechaContrato);
+            break;
+          }
+        }
+        this.myForm.controls.personaId.setValidators([Validators.requiredTrue]);
+        this.myForm.controls.personaId.updateValueAndValidity();
+        if (encontrado) {
+          this.myForm.controls.personaId.clearValidators();
+          this.myForm.controls.personaId.updateValueAndValidity();
+        }
       }
+    }
+
+    console.log("La persona elegida es ",this.myForm.value);
   }
 
 }
