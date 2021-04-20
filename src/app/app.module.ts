139e1784d2db49914c8e94f041a7a9fe86d3bdb0
santiago mariscal <santiago.mariscal@advisoryservicesg.tech>
2021-04-19 19:18:03 -0500

    cambios enn persona

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { LOCALE_ID, NgModule } from '@angular/core';
+import { DEFAULT_CURRENCY_CODE, LOCALE_ID, NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -23,6 +23,7 @@ import { InterceptorService } from './core/interceptores/interceptor.service';
 import localeEn from '@angular/common/locales/es-MX';
 import { registerLocaleData } from '@angular/common';
 registerLocaleData(localeEn, 'es-MX');
+
 
 @NgModule({
   declarations: [
@@ -48,7 +49,7 @@ registerLocaleData(localeEn, 'es-MX');
     },
     {
       provide:LOCALE_ID,useValue:'es-MX'
-    }],
+    },{provide: DEFAULT_CURRENCY_CODE,useValue:''}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
