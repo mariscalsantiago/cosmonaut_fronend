7ca2bcfb78770a659b001594536dff90c65974cc
Raul Jamaica Rosas <rjamaica@advisoryservicesg.tech>
2021-05-11 10:21:37 -0500

    Corecciones

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ export const environment = {
   rutaEmpresa:'https://34.75.54.120/empresa',
   rutaCatalogos:'https://34.75.54.120/catalogos',
   rutaCalculos:'http://c5f8d32b57a5.ngrok.io',
-  rutaTablaValores:'http://1a1f95a24aac.ngrok.io'
+  rutaTablaValores:'https://34.75.54.120/admin'
 };
