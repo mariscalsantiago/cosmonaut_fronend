4290a58f2dba3da879a472f647313e41bf8d443f
Raul Jamaica Rosas <rjamaica@advisoryservicesg.tech>
2021-05-17 11:40:55 -0500

    KArdex

--- src/app/modules/empleados/pages/submodulos/kardex/pages/kardex/kardex.component.ts
+++ src/app/modules/empleados/pages/submodulos/kardex/pages/kardex/kardex.component.ts
@@ -1,4 +1,9 @@
 import { Component, HostListener, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { tabla } from 'src/app/core/data/tabla';
+import { DatePipe } from '@angular/common';
+import { KardexService } from 'src/app/modules/empleados/services/kardex.service';
+import { UsuarioSistemaService } from 'src/app/shared/services/usuariosistema/usuario-sistema.service';
 
 @Component({
   selector: 'app-kardex',
@@ -16,39 +21,74 @@ export class KardexComponent implements OnInit {
 
   public tamanio:number = 0;
   public cargando:Boolean = false;
+  public arreglo: any = [];
+  public idEmpleado: number = 0;
 
   public arreglotabla:any = {
     columnas:[],
     filas:[]
   };
 
-  
-  constructor() { }
+  public arreglotablaDesglose: any = {
+    columnas: [],
+    filas: []
+  };
+
+  constructor(private router:ActivatedRoute, private ksrdexPrd: KardexService, 
+    private usuariosSistemaPrd:UsuarioSistemaService) { }
+
 
   ngOnInit(): void {
 
     let documento:any = document.defaultView;
     this.tamanio = documento.innerWidth;
 
-    this.cargando = true;
+    this.router.params.subscribe(params => {
+      this.idEmpleado = params["id"];
+    });
 
-        /*this.companyProd.getAll().subscribe(datos =>{
-          this.arreglo = datos.datos;
-          let columnas:Array<tabla> = [
-            new tabla("url","imagen"),
-            new tabla("centrocClienteId","ID empresa"),
-            new tabla("razonSocial","Razón social	"),
-            new tabla("nombre","Nombre de la empresa	"),
-            new tabla("rfc","RFC"),
-            new tabla("fechaAlta","Fecha de registro en el sistema"),
-            new tabla("esActivo","Estatus")
-          ]
-          this.arreglotabla.columnas = columnas;
-          this.arreglotabla.filas = this.arreglo;
-          this.cargando = false;
-      });*/
+    this.cargando = true;
+    this.ksrdexPrd.getListaDocumentosEmpleado(this.usuariosSistemaPrd.getIdEmpresa(),this.idEmpleado).subscribe(datos => {
+        this.crearTabla(datos);
+    });
 
   }
+
+
+
+  public crearTabla(datos:any){
+    
+    this.arreglo = datos.datos;
+
+    
+    let columnas: Array<tabla> = [
+      new tabla("nombreArchivo", "Nombre"),
+      new tabla("fechaCargaDocumento", "Fecha"),
+      new tabla("tipoDocumento", "Tipo de documento")
+    ]
+
+
+    this.arreglotabla = {
+      columnas:[],
+      filas:[]
+    }
+
+
+    if(this.arreglo !== undefined){
+      for(let item of this.arreglo){
+        item.fechaCarga = (new Date(item.fechaCarga).toUTCString()).replace(" 00:00:00 GMT", "");
+        let datepipe = new DatePipe("es-MX");
+        item.fechaCargaDocumento = datepipe.transform(item.fechaCarga , 'dd-MMM-y')?.replace(".","");
+
+        item.tipoDocumento= item.tipoDocumento?.nombre;
+      }
+    }
+
+    this.arreglotabla.columnas = columnas;
+    this.arreglotabla.filas = this.arreglo;
+    this.cargando = false;
+  }
+
   public filtrar() {
   
   }
