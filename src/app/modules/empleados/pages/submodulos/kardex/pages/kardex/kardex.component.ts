28b10869e9a703b56fe514215d74881622f6b7fd
Raul Jamaica Rosas <rjamaica@advisoryservicesg.tech>
2021-05-17 18:00:39 -0500

    SUA

--- src/app/modules/empleados/pages/submodulos/kardex/pages/kardex/kardex.component.ts
+++ src/app/modules/empleados/pages/submodulos/kardex/pages/kardex/kardex.component.ts
@@ -1,9 +1,12 @@
-import { Component, HostListener, OnInit } from '@angular/core';
+import { Component, HostListener, ElementRef, OnInit, ViewChild, ViewChildren } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
+import { KardexService } from 'src/app/modules/empleados/services/kardex.service';
+import { ModalService } from 'src/app/shared/services/modales/modal.service';
 import { tabla } from 'src/app/core/data/tabla';
 import { DatePipe } from '@angular/common';
-import { KardexService } from 'src/app/modules/empleados/services/kardex.service';
 import { UsuarioSistemaService } from 'src/app/shared/services/usuariosistema/usuario-sistema.service';
+
 
 @Component({
   selector: 'app-kardex',
@@ -11,49 +14,51 @@ import { UsuarioSistemaService } from 'src/app/shared/services/usuariosistema/us
   styleUrls: ['./kardex.component.scss']
 })
 export class KardexComponent implements OnInit {
-  @HostListener('window:resize', ['$event'])
-  onResize(event: any) {
-    event.target.innerWidth;
-
-
-    this.tamanio = event.target.innerWidth;
-  }
 
   public tamanio:number = 0;
-  public cargando:Boolean = false;
+  public cargando : Boolean = false;
   public arreglo: any = [];
+  public cargandoIcon: boolean= false;
   public idEmpleado: number = 0;
+  public idEmpresa: number = 0;
 
+  
   public arreglotabla:any = {
     columnas:[],
     filas:[]
   };
 
-  public arreglotablaDesglose: any = {
-    columnas: [],
-    filas: []
-  };
+  public arreglotablaDesglose:any = {
+    columnas:[],
+    filas:[]
+};
 
-  constructor(private router:ActivatedRoute, private ksrdexPrd: KardexService, 
-    private usuariosSistemaPrd:UsuarioSistemaService) { }
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    event.target.innerWidth;
+    this.tamanio = event.target.innerWidth;
+  }
 
+
+
+  constructor(private routerPrd: Router, private kardexPrd: KardexService,private modalPrd:ModalService, 
+    private router:ActivatedRoute,private usuariosSistemaPrd:UsuarioSistemaService) { }
 
   ngOnInit(): void {
 
-    let documento:any = document.defaultView;
-    this.tamanio = documento.innerWidth;
-
+    
+    debugger;
     this.router.params.subscribe(params => {
       this.idEmpleado = params["id"];
     });
-
+  
     this.cargando = true;
-    this.ksrdexPrd.getListaDocumentosEmpleado(this.usuariosSistemaPrd.getIdEmpresa(),this.idEmpleado).subscribe(datos => {
+    this.kardexPrd.getListaDocumentosEmpleado(this.usuariosSistemaPrd.getIdEmpresa(),this.idEmpleado).subscribe(datos => {
         this.crearTabla(datos);
     });
 
-  }
 
+  }
 
 
   public crearTabla(datos:any){
@@ -62,9 +67,9 @@ export class KardexComponent implements OnInit {
 
     
     let columnas: Array<tabla> = [
-      new tabla("nombreArchivo", "Nombre"),
-      new tabla("fechaCargaDocumento", "Fecha"),
-      new tabla("tipoDocumento", "Tipo de documento")
+      new tabla("nombreArchivo", "Tipo de movimiento"),
+      new tabla("fechaCargaDocumento", "Fecha de movimiento"),
+      new tabla("tipoDocumento", "Registro patronal")
     ]
 
 
@@ -89,8 +94,47 @@ export class KardexComponent implements OnInit {
     this.cargando = false;
   }
 
-  public filtrar() {
   
+
+  public recibirTabla(obj:any){
+    if (obj.type == "editar") {
+      let datos = obj.datos;
+    }
+    if (obj.type == "descargar") {
+    }
+    if (obj.type == "desglosar") {
+
+            let item = obj.datos;
+
+   
+    
+              let columnas: Array<tabla> = [
+                new tabla("descripcion", "Política"),
+                new tabla("funcionCuenta", "Salario diario"),
+                new tabla("numInformacion", "Salario diario integrado"),
+                new tabla("numInformacion", "Salario base de cotización"),
+                new tabla("numSucursal", "Eatatus Empleado")
+              ];
+              
+              item.funcionCuenta = item.funcionCuentaId?.descripcion;
+    
+    
+              this.arreglotablaDesglose.columnas = columnas;
+              this.arreglotablaDesglose.filas = item;
+    
+              item.cargandoDetalle = false;
+    
+            }
+
   }
+
+
+  public filtrar(){
+
+  }
+
+
+
+
 
 }
