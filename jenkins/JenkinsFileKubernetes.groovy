c6f2c4ca19a44a95a62b82e6487606fbe7cb7d75
Isidoro Trevino <isidoro.trevino@gmail.com>
2021-07-08 11:47:11 -0500

    Config de k8s

---
+++ jenkins/JenkinsFileKubernetes.groovy
@@ -0,0 +1,36 @@
+podTemplate(containers:[
+    containerTemplate(name: 'gradle', image: 'gradle:6.7.0-jdk11-hotspot', command: 'sleep', args: '99d'),
+    containerTemplate(name: 'docker-registry', image: 'gcr.io/kaniko-project/executor:v1.6.0-debug', command: 'sleep', args: '99d'),
+    containerTemplate(name: 'container-registry', image: 'gcr.io/google.com/cloudsdktool/cloud-sdk:327.0.0', command: 'sleep', args: '99d'),
+    containerTemplate(name: 'protractor', image: 'wintermutetec/protractor:v5.4.x', command: 'sleep', args: '99d'),
+    containerTemplate(name: 'angular', image: 'teracy/angular-cli:8.3', command: 'sleep', args: '99d')    
+  ], volumes: [
+  persistentVolumeClaim(mountPath: '/root/.m2/repository', claimName: 'maven-repo', readOnly: false),
+  persistentVolumeClaim(mountPath: '/root/.gradle', claimName: 'gradle-repo', readOnly: false),
+    secretVolume(secretName: 'k8s-sql-secret', mountPath: '/secret')
+  ],envVars:[envVar(key:'GOOGLE_APPLICATION_CREDENTIALS',value:'/secret/service_account.json')] ) {
+  node(POD_LABEL) {
+    stage("Compilacion") {
+        container('angular'){
+            script{
+			   git url: 'https://github.com/ASG-BPM/cosmonaut_admin',branch:'main',credentialsId: 'winter_user'
+			   tag = sh(script:'git describe --tags --always `git rev-list --tags` | grep DEV | head -1',returnStdout: true ).trim()
+			   sh "git checkout $tag"
+			   sh 'npm install'
+               sh 'ng build --prod --base-href /cosmonaut-front/'
+		   }
+        }
+     }     
+     stage('Deploy to GoogleStorage') {
+        container('container-registry'){
+            script{
+                sh 'gcloud auth activate-service-account cosmonaut-k8s-qa-sa@cosmonaut-qa.iam.gserviceaccount.com --key-file=/secret/service_account.json'
+				sh 'cd dist/cosmonaut-front/ && gsutil cp -r *  gs://qa-cosmonaut-public-front/'		    
+		     }
+        }
+     }
+     }     
+  }
+  
+
+
