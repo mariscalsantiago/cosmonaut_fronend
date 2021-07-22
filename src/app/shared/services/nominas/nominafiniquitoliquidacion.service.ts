0a560a87b6cf1cf19af04e7d042be9edde85d295
santiago mariscal <santiago.mariscal@advisoryservicesg.tech>
2021-07-22 11:34:48 -0500

    nominas

--- src/app/shared/services/nominas/nominafiniquitoliquidacion.service.ts
+++ src/app/shared/services/nominas/nominafiniquitoliquidacion.service.ts
@@ -11,150 +11,86 @@ export class NominafiniquitoliquidacionService {
   constructor(private http: HttpClient) { }
 
   public crearNomina(obj: any): Observable<any> {
-    const httpOptions = {
-      headers: new HttpHeaders({
-        'Content-Type': 'application/json'
-      })
-    };
     let json: string = JSON.stringify(obj);
-    return this.http.post(`${direcciones.nominaLiquidacion}/guardar/nomina/finiquito-liquidacion`, json, httpOptions);
+    return this.http.post(`${direcciones.nominaLiquidacion}/guardar/nomina/finiquito-liquidacion`, json);
   }
   public calcularNomina(obj:any):Observable<any> {
-    const httpOptions = {
-      headers: new HttpHeaders({
-        'Content-Type': 'application/json'
-      })
-    };
     let json: string = JSON.stringify(obj);
    // return this.creandoObservable(finiquitoCalculo);
-    return this.http.post(`${direcciones.nominaLiquidacion}/calcula/nomina/finiquito-liquidacion`, json, httpOptions);
+    return this.http.post(`${direcciones.nominaLiquidacion}/calcula/nomina/finiquito-liquidacion`, json);
 
   }
 
   public getListaNominas(obj:any):Observable<any> {
-    const httpOptions = {
-      headers: new HttpHeaders({
-        'Content-Type': 'application/json'
-      })
-    };
     let json: string = JSON.stringify(obj);
 //  return this.creandoObservable(finiquitoListaActivos);
-    return this.http.post(`${direcciones.nominaLiquidacion}/lista/nomina/liquidacion`, json, httpOptions);
+    return this.http.post(`${direcciones.nominaLiquidacion}/lista/nomina/liquidacion`, json);
 
   }
 
 
   
   public getUsuariosCalculados(obj:any):Observable<any> {
-    const httpOptions = {
-      headers: new HttpHeaders({
-        'Content-Type': 'application/json'
-      })
-    };
     let json: string = JSON.stringify(obj);
-   // return this.creandoObservable(finiquitoUsuariosCalculados);
-    return this.http.post(`${direcciones.nominaLiquidacion}/lista/empleados/total/pago/neto/liquidacion`, json, httpOptions);
-
+    //return this.creandoObservable(finiquitoUsuariosCalculados);
+    return this.http.post(`${direcciones.nominaLiquidacion}/lista/empleados/total/pago/neto/liquidacion`, json);
   }
 
   public getUsuariosCalculadosFiltrado(obj:any):Observable<any> {
-    const httpOptions = {
-      headers: new HttpHeaders({
-        'Content-Type': 'application/json'
-      })
-    };
     let json: string = JSON.stringify(obj);
    // return this.creandoObservable(finiquitoUsuariosCalculados);
-    return this.http.post(`${direcciones.nominaLiquidacion}/lista/empleados/total/pago/neto/liquidacion/filtrar`, json, httpOptions);
+    return this.http.post(`${direcciones.nominaLiquidacion}/lista/empleados/total/pago/neto/liquidacion/filtrar`, json);
 
   }
 
 
   public getUsuariosCalculadosDetalle(obj:any):Observable<any> {
-    const httpOptions = {
-      headers: new HttpHeaders({
-        'Content-Type': 'application/json'
-      })
-    };
     let json: string = JSON.stringify(obj);
    // return this.creandoObservable(finiquitoUsuariosCalculadosDetalle);
-   return this.http.post(`${direcciones.nominaLiquidacion}/detalle/liquidacion/empleado`, json, httpOptions);
+   return this.http.post(`${direcciones.nominaLiquidacion}/detalle/liquidacion/empleado`, json);
 
   }
 
   public getUsuariosDispersion(obj:any):Observable<any> {
-
-    const httpOptions = {
-      headers: new HttpHeaders({
-        'Content-Type': 'application/json'
-      })
-    };
     let json: string = JSON.stringify(obj);
    // return this.creandoObservable(finiquitoUsuariosDispersion);
-   return this.http.post(`${direcciones.nominaLiquidacion}/lista/empleados/fechabaja/percepciones/deducciones/liquidacion`, json, httpOptions);
+   return this.http.post(`${direcciones.nominaLiquidacion}/lista/empleados/fechabaja/percepciones/deducciones/liquidacion`, json);
 
   }
 
   public getUsuariosDispersionFiltrar(obj:any):Observable<any> {
-
-    const httpOptions = {
-      headers: new HttpHeaders({
-        'Content-Type': 'application/json'
-      })
-    };
     let json: string = JSON.stringify(obj);
    // return this.creandoObservable(finiquitoUsuariosDispersion);
-   return this.http.post(`${direcciones.nominaLiquidacion}/lista/empleados/fechabaja/percepciones/deducciones/liquidacion/filtrar`, json, httpOptions);
+   return this.http.post(`${direcciones.nominaLiquidacion}/lista/empleados/fechabaja/percepciones/deducciones/liquidacion/filtrar`, json);
 
   }
 
 
   public getUsuariosTimbrado(obj:any):Observable<any> {
-
-    const httpOptions = {
-      headers: new HttpHeaders({
-        'Content-Type': 'application/json'
-      })
-    };
     let json: string = JSON.stringify(obj);
    // return this.creandoObservable(finiquitoUsuariosTimbrado);
-    return this.http.post(`${direcciones.nominaLiquidacion}/lista/empleados/total/pagoneto/timbrado/liquidacion`, json, httpOptions);
+    return this.http.post(`${direcciones.nominaLiquidacion}/lista/empleados/total/pagoneto/timbrado/liquidacion`, json);
 
   }
 
   public getUsuariosTimbradoFiltrar(obj:any):Observable<any> {
-
-    const httpOptions = {
-      headers: new HttpHeaders({
-        'Content-Type': 'application/json'
-      })
-    };
     let json: string = JSON.stringify(obj);
    // return this.creandoObservable(finiquitoUsuariosTimbrado);
-    return this.http.post(`${direcciones.nominaLiquidacion}/lista/empleados/total/pagoneto/timbrado/liquidacion/filtrar`, json, httpOptions);
+    return this.http.post(`${direcciones.nominaLiquidacion}/lista/empleados/total/pagoneto/timbrado/liquidacion/filtrar`, json);
 
   }
 
   public getUsuariosTimbradoDetalle(obj:any):Observable<any> {
-    const httpOptions = {
-      headers: new HttpHeaders({
-        'Content-Type': 'application/json'
-      })
-    };
     let json: string = JSON.stringify(obj);
    // return this.creandoObservable(finiquitoUsuariosTimbradoDetalle);
-    return this.http.post(`${direcciones.nominaLiquidacion}/detalle/empleados/total/pagoneto/detalle/montos/timbrado/liquidacion`, json, httpOptions);
+    return this.http.post(`${direcciones.nominaLiquidacion}/detalle/empleados/total/pagoneto/detalle/montos/timbrado/liquidacion`, json);
 
   }
 
   public eliminar(obj:any):Observable<any>{
-    const httpOptions = {
-      headers: new HttpHeaders({
-        'Content-Type': 'application/json'
-      })
-    };
+  
     let json: string = JSON.stringify(obj);
-    return this.http.post(`${direcciones.nominaLiquidacion}/eliminacion/nomina/liquidacion`, json, httpOptions);
+    return this.http.post(`${direcciones.nominaLiquidacion}/eliminacion/nomina/liquidacion`, json);
 
   }
 
