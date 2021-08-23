49b2bbb25768ee582d3acf4330c763d730000618
santiago Antonio Mariscal Velasquez <samvprogrammer@Air-de-santiago.lan>
2021-08-22 21:28:35 -0500

    fecha corregida

--- src/app/shared/tag/tag.component.ts
+++ src/app/shared/tag/tag.component.ts
@@ -126,9 +126,11 @@ export class TagComponent implements OnInit,OnChanges {
       if(this.arregloEtiquetas.length >= Number(this.dias)){
           return;
       }
-
-      console.log("Esta es la fecha",this.elementoSeleccionado);
-
+      let encontrado:boolean = this.arregloEtiquetas.some((o:any)=> o == this.elementoSeleccionado)
+      if(encontrado){
+        this.elementoSeleccionado = "";
+        return;
+      };
       this.arregloEtiquetas.push(this.elementoSeleccionado);
       this.elementoSeleccionado = "";
       this.salida.emit(this.arregloEtiquetas);
