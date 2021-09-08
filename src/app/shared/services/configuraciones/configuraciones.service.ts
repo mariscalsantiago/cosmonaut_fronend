a6f9d65634846e0f220af963849978df12f85bec
Raul Jamaica Rosas <rjamaica@advisoryservicesg.tech>
2021-09-08 18:17:34 -0500

    Correciones

--- src/app/shared/services/configuraciones/configuraciones.service.ts
+++ src/app/shared/services/configuraciones/configuraciones.service.ts
@@ -50,7 +50,8 @@ export class ConfiguracionesService {
   private secretKey: string = "llavesecreta@por@santiagoantoniomariscal";
 
   public static regexCurp = /^([A-Z][AEIOUX][A-Z]{2}\d{2}(?:0[1-9]|1[0-2])(?:0[1-9]|[12]\d|3[01])[HM](?:AS|B[CS]|C[CLMSH]|D[FG]|G[TR]|HG|JC|M[CNS]|N[ETL]|OC|PL|Q[TR]|S[PLR]|T[CSL]|VZ|YN|ZS)[B-DF-HJ-NP-TV-Z]{3}[A-Z\d])(\d)$/;
-  public static regexRFC = '^([A-ZÑ\x26]{3,4}([0-9]{2})(0[1-9]|1[0-2])(0[1-9]|1[0-9]|2[0-9]|3[0-1])([A-Z]|[0-9]){2}([A]|[0-9]){1})?$';
+  //public static regexRFC = '^([A-ZÑ\x26]{2,4}([0-9]{2})(0[1-9]|1[0-2])(0[1-9]|1[0-9]|2[0-9]|3[0-1])([A-Z]|[0-9]){2}([A]|[0-9]){1})?$';
+  public static regexRFC = /^([A-Z,Ñ,&]{2,4}([0-9]{2})(0[1-9]|1[0-2])(0[1-9]|1[0-9]|2[0-9]|3[0-1])[A-Z|\d]{2,3})$/;
 
 
 
