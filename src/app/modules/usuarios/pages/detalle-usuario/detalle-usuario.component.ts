9a8e50c7111c6e5e6515d36b843e14741e1e652c
santiago Antonio Mariscal Velasquez <samvprogrammer@Air-de-santiago.lan>
2021-10-25 17:13:49 -0500

    detalle usuario

--- src/app/modules/usuarios/pages/detalle-usuario/detalle-usuario.component.ts
+++ src/app/modules/usuarios/pages/detalle-usuario/detalle-usuario.component.ts
@@ -93,7 +93,16 @@ export class DetalleUsuarioComponent implements OnInit {
             this.arregloRoles.push(this.objusuario.rolId);
           }
         }
+       
       }, 200);
+
+      if(this.insertar){
+        if(this.usuariosSistemaPrd.getVersionSistema() == 1){
+          let  rolid = this.myForm.value.rol;
+          
+          this.desabilitarAgregarUsuarios(rolid);
+      }
+      }
 
     });
 
@@ -105,13 +114,12 @@ export class DetalleUsuarioComponent implements OnInit {
     }
 
 
+
     this.myForm = this.createForm(this.objusuario);
 
     if (!this.insertar && this.esClienteEmpresa) {
       let companiaSeleccionada = this.arregloCompany.find((o: any) => o["centrocClienteId"] == this.myForm.controls.centrocClienteId.value);
       this.myForm.controls.centrocClienteId.setValue(companiaSeleccionada.razonSocial);
-
-      this.desabilitarTodo 
 
     }
 
@@ -147,6 +155,20 @@ export class DetalleUsuarioComponent implements OnInit {
 
   }
 
+  public desabilitarAgregarUsuarios(idRol:number){
+    debugger;
+    if(idRol == 1){
+      this.myForm.controls.multicliente.enable();
+      this.myForm.controls.centrocClienteId.setValue("");
+      this.myForm.controls.centrocClienteId.enable();
+    }else{
+      this.myForm.controls.multicliente.disable();
+      this.myForm.controls.centrocClienteId.setValue("GA");
+      this.myForm.controls.centrocClienteId.disable();
+
+    }
+  }
+
   public desabilitarInputs():void{
     this.myForm.controls.nombre.disable();
     this.myForm.controls.apellidoPaterno.disable();
@@ -159,6 +181,7 @@ export class DetalleUsuarioComponent implements OnInit {
 
   public suscripciones() {
     this.myForm.controls.rol.valueChanges.subscribe(valor => {
+      debugger;
       if (this.usuariosSistemaPrd.esCliente() && this.usuariosSistemaPrd.getVersionSistema() == 1) {
         if (valor != 1) {
           this.myForm.controls.multicliente.disable();
@@ -168,11 +191,17 @@ export class DetalleUsuarioComponent implements OnInit {
           this.myForm.controls.centrocClienteId.disable();
           return;
         }
+      }else if(this.esClienteEmpresa && this.insertar && this.usuariosSistemaPrd.getVersionSistema() == 1){
+        let  rolid = this.myForm.value.rol;
+          this.desabilitarAgregarUsuarios(rolid);
       }
 
 
       this.myForm.controls.multicliente.enable();
       this.myForm.controls.centrocClienteId.enable();
+
+
+
     });
   }
 
