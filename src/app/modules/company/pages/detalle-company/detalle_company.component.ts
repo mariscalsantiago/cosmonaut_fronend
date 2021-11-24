82f340cfc87063db5b9cbbdaa6c9a2de21416074
Raul Jamaica Rosas <rjamaica@advisoryservicesg.tech>
2021-11-23 19:36:46 -0600

    Correcciones fechas formato toda la aplicacion

--- src/app/modules/company/pages/detalle-company/detalle_company.component.ts
+++ src/app/modules/company/pages/detalle-company/detalle_company.component.ts
@@ -143,7 +143,7 @@ export class DetalleCompanyComponent implements OnInit {
       nombre: [obj.nombre, [Validators.required]],
       razonSocial: [obj.razonSocial, [Validators.required]],
       rfc: [obj.rfc, [Validators.required, Validators.pattern(ConfiguracionesService.regexRFC)]],
-      fechaAlta: [{ value: ((this.insertar) ? datePipe.transform(new Date(), 'dd/MM/yyyy') : obj.fechaAlta), disabled: true }, [Validators.required]],
+      fechaAlta: [{ value: ((this.insertar) ? datePipe.transform(new Date(), 'dd-MMM-y') : obj.fechaAlta), disabled: true }, [Validators.required]],
       esActivo: [{ value: (this.insertar) ? "true" : obj.esActivo, disabled: this.insertar }, [Validators.required]],
       centrocClienteId: obj.centrocClienteId,
       multiempresa: obj.multiempresa,
